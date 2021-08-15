abbd40a88281d5af9502904f33eee3d937422ca3
BubuDavid <david.pedroza.segoviano@gmail.com>
2021-08-14 19:25:08 -0500

    Adding my version

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import oyaml as yaml
 from flask import Flask
 from flask import render_template
@@ -8,9 +9,10 @@ app.config['JSON_SORT_KEYS'] = False
 
 @app.route('/')
 def index():
-    website_data = yaml.load(open('_config.yaml'))
+    website_data = yaml.load(open('_config.yaml', encoding='UTF-8'))
 
-    return render_template('index.html', data=website_data)
+    print(website_data['profile'])
+    return render_template('index.html', data=website_data, allow_unicode=True)
 
 
 if __name__ == '__main__':
